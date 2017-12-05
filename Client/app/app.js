cf13993f55799213751b906387b17ad64f3c9d29
mcdonndi <mcdonndi@tcd.ie>
2017-12-05 23:56:54 +0000

    Looping application unti quit string

--- Client/app/app.js
+++ Client/app/app.js
@@ -1,48 +1,54 @@
 const readline = require('readline');
 const API = require("../api/api.js");
 
-//let api = new API();
-
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-rl.question('Hello. What would you like to do?\n1) Open file\n2) Close file\n3) Read file\n4) Write to file\n', (choice) => {
-    switch(choice){
+rl.setPrompt('Hello. What would you like to do?\n1) Open file\n2) Close file\n3) Read file\n4) Write to file\n');
+rl.prompt();
+
+rl.on('line', (choice) => {
+    switch (choice) {
         case "1":
             console.log("Option 1 selected");
-            getFilePath(rl, (filePath) =>{
+            getFilePath((filePath) => {
                 API.openFile(filePath);
             });
             break;
         case "2":
             console.log("Option 2 selected");
-            getFilePath(rl, (filePath) => {
+            getFilePath((filePath) => {
                 API.closeFile(filePath);
             });
             break;
         case "3":
             console.log("Option 3 selected");
-            getFilePath(rl, (filePath) => {
+            getFilePath((filePath) => {
                 API.readFile(filePath);
             });
             break;
         case "4":
             console.log("Option 4 selected");
-            getFilePath(rl, (filePath) => {
+            getFilePath((filePath) => {
                 API.writeFile(filePath);
             });
             break;
+        case "quit":
+            rl.close();
+            break;
         default:
             console.log("Invalid selection!");
-            rl.close();
     }
+    rl.prompt();
+}).on('close', () => {
+    console.log('Have a great day!');
+    process.exit(0);
 });
 
-getFilePath = (rl, cb) => {
+getFilePath = (cb) => {
     rl.question('Please enter file path: ', (fPath) => {
         cb(fPath);
-        rl.close();
     });
 };
