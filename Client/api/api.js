8b2f43f4c8eda305dd0ac33f289aa008e92795bc
mcdonndi <mcdonndi@tcd.ie>
2017-12-07 22:25:55 +0000

    Client recieving file from FS through DS

--- Client/api/api.js
+++ Client/api/api.js
@@ -5,18 +5,20 @@ const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 class API {
     static openFile(filePath) {
         console.log("Opening file: " + filePath);
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', "http://localhost:8000" + filePath, true);
-        xhr.send();
+        this._getFileServerPort(filePath, (portNumber) => {
+            let xhr = new XMLHttpRequest();
+            xhr.open('GET', `http://localhost:${portNumber}/${filePath}`, true);
+            xhr.send();
 
-        xhr.addEventListener("readystatechange", processRequest, false);
+            xhr.addEventListener("readystatechange", processRequest, false);
 
-        function processRequest(e) {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                //let response = JSON.parse(xhr.responseText);
-                console.log(xhr.responseText);
+            function processRequest(e) {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    //let response = JSON.parse(xhr.responseText);
+                    console.log(xhr.responseText);
+                }
             }
-        }
+        });
     }
 
     static closeFile(filePath) {
@@ -29,6 +31,24 @@ class API {
 
     static writeFile(filePath) {
         console.log("Writing to file: " + filePath);
+    }
+
+    static _getFileServerPort(filePath, cb) {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', "http://localhost:8000/" + filePath, true);
+        xhr.send();
+
+        xhr.addEventListener("readystatechange", processRequest, false);
+
+        function processRequest(e) {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                console.log(`File server port number: ${xhr.responseText}`);
+                cb(xhr.responseText);
+            }
+            else {
+                cb(null);
+            }
+        }
     }
 }
 
